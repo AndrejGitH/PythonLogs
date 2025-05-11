6f23c33e2518c488c3fefab25ffd0380223b44d7
Andrej <andrej.chupan@gmail.com>
2025-05-11 22:56:38 +0200

    Strucutre and duplicates

--- MergeLogs.py
+++ MergeLogs.py
@@ -6,75 +6,113 @@
 #Write a unique timestamp first, then search in dictionary if there is such key, if yes, write log detail in same row, if not, wriet N/A
 
 import os
+import sys
 
+def get_paths():
 #User provided path
-file_paths = input("Enter file paths (separated by spaces): ").split()
-#List to hold all dictionaries which contain file data
-all_logs = []
+    file_paths = input("Enter file paths (separated by spaces): ").split()
 
-#Algo for getting data into the list
-for path in file_paths:
-    file_dictionary = {}
+    file_paths = list(set([p.strip() for p in file_paths if p.strip()]))
+        
+    if not file_paths:
+        print("No file path entered!")
+        sys.exit()
+    return file_paths
 
-    try:
-        with open(path, 'r') as file:
-            for line in file:
-                line = line.strip()
-                if not line:
-                    continue
-                space_index = line.find(' ')
-                if space_index != -1:
-                    timestamp = line[:space_index]
-                    message = line[space_index+1:]
-                    #If multiple logs at the same time, append them together
-                    if timestamp in file_dictionary:
-                        file_dictionary[timestamp].append(message)
-                    else:
-                        file_dictionary[timestamp] = [message]
 
-        all_logs.append(file_dictionary)
+def get_log_files(file_paths):
+
     
-    except FileNotFoundError:
-        print(f"File not found: {path}")
+    #List to hold all dictionaries which contain file data
+    logs_list = []
 
-#Store all unique timestamps
-all_timestamps = set()
-for file in all_logs:
-    for timestamp in file.keys():
-        all_timestamps.add(timestamp)
-
-#Sorting unique timestamps
-sorted_timestamps = sorted(all_timestamps)
-
-#Set column width
-timestamp_width = 13
-max_length = 0
-
-#Find the longest message
-for log in all_logs:
-    for message in log.values():
-        appended = ";".join(message)
-        max_length = max(max_length, len(appended)) 
-
-text_width = max(max_length + 5,5)
-
-#Write header to the output file
-with open("mergedLogs.txt", "w") as output:
-    output.write("Timestamp".ljust(timestamp_width))
+    #Algo for getting data into the list
     for path in file_paths:
-        output.write("| " + os.path.basename(path).ljust(text_width))
-    output.write("\n")
+        file_dictionary = {}
 
-#Check if the list contain particular timestamp, if not, write N/A
-    for time in sorted_timestamps:
-        output.write(time.ljust(timestamp_width))
-        for file in all_logs:
-            if time in file:
-                messages = ";".join(file[time])
-                output.write("| " + messages.ljust(text_width))
-            else:
-                output.write("| "+ "N/A".ljust(text_width))
+        try:
+            with open(path, 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    if not line:
+                        continue
+                    space_index = line.find(' ')
+                    if space_index != -1:
+                        timestamp = line[:space_index]
+                        message = line[space_index+1:]
+                        #If multiple logs at the same time, append them together
+                        if timestamp in file_dictionary:
+                            file_dictionary[timestamp].append(message)
+                        else:
+                            file_dictionary[timestamp] = [message]
 
+            logs_list.append(file_dictionary)
+        
+        except FileNotFoundError:
+            print(f"File not found: {path}")
+        except Exception as ex:
+            print("Error occured! Details: ", ex)
+
+    if not logs_list:
+        print("No valid logs to process! Exiting the program.")
+        sys.exit()
+
+    return logs_list
+
+def get_timestamps(logs_list):
+    #Store all unique timestamps
+    all_timestamps = set()
+    for file in logs_list:
+        for timestamp in file.keys():
+            all_timestamps.add(timestamp)
+    #Sorting unique timestamps
+    sorted_timestamps = sorted(all_timestamps)
+    return sorted_timestamps
+
+
+
+def get_longest_message(logs_list):
+#Find the longest message
+    max_length = 0
+    for log in logs_list:
+        for message in log.values():
+            appended = ";".join(message)
+            max_length = max(max_length, len(appended)) 
+
+    return max(max_length + 5,5)
+
+def get_merged_logs(file_paths, logs_list, sorted_timestamps):
+
+    #Set column width
+    timestamp_width = 13
+    text_width = get_longest_message(logs_list)
+
+    #Write header to the output file
+    with open("mergedLogs.txt", "w") as output:
+        output.write("Timestamp".ljust(timestamp_width))
+        for path in file_paths:
+            output.write("| " + os.path.basename(path).ljust(text_width))
         output.write("\n")
 
-print("Merging successfull!")
+    #Check if the list contain particular timestamp, if not, write N/A
+        for time in sorted_timestamps:
+            output.write(time.ljust(timestamp_width))
+            for file in logs_list:
+                if time in file:
+                    messages = ";".join(file[time])
+                    output.write("| " + messages.ljust(text_width))
+                else:
+                    output.write("| "+ "N/A".ljust(text_width))
+
+            output.write("\n")
+
+    print("Merging successfull!")
+
+def main():
+        paths = get_paths()
+        logs = get_log_files(paths)
+        sorted_timestamps = get_timestamps(logs)
+        get_merged_logs(paths,logs,sorted_timestamps)
+
+if __name__ == "__main__":
+    main()
